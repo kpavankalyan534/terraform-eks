b301aaa799b874d56b1173491ee3421afba7bf2c
Pavan Kalyan <kpavankalyan534@gmail.com>
2024-12-20 16:21:19 +0530

    fix

---
+++ s3.tf
@@ -0,0 +1,48 @@
+# Define S3 Bucket
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "pavan-terraform-state"  # Change to a unique name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Enable Versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable Encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block Public Access for the S3 bucket
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Create DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_state" {
+  name         = "terraform-state"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
