1b65193d3aa29f6568a5935f88f2e75b4c51efd8
Pavan Kalyan <kpavankalyan534@gmail.com>
2024-12-20 11:29:56 +0530

    revert

--- iam-autoscaler.tf
+++ iam-autoscaler.tf
@@ -5,7 +5,7 @@ data "aws_iam_policy_document" "eks_cluster_autoscaler_assume_role_policy" {
 
     condition {
       test     = "StringEquals"
-      variable = replace(aws_iam_openid_connect_provider.eks.url, "https://", "") + ":sub"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
       values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
     }
 
@@ -25,21 +25,19 @@ resource "aws_iam_policy" "eks_cluster_autoscaler" {
   name = "eks-cluster-autoscaler"
 
   policy = jsonencode({
-    Statement = [
-      {
-        Action   = [
-          "autoscaling:DescribeAutoScalingGroups",
-          "autoscaling:DescribeAutoScalingInstances",
-          "autoscaling:DescribeLaunchConfigurations",
-          "autoscaling:DescribeTags",
-          "autoscaling:SetDesiredCapacity",
-          "autoscaling:TerminateInstanceInAutoScalingGroup",
-          "ec2:DescribeLaunchTemplateVersions"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
+    Statement = [{
+      Action = [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "ec2:DescribeLaunchTemplateVersions"
+            ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
     Version = "2012-10-17"
   })
 }
