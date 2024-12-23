ec2209c2a7955cccd893eac729da4886bfd93a06
Pavan Kalyan <kpavankalyan534@gmail.com>
2024-12-23 22:49:53 +0530

    revert

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "test-terraform-state"
+    bucket         = "pavan-terraform-state"
     key            = "global/s3/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "test-terraform-state"
+    dynamodb_table = "terraform-state"
   }
 }
