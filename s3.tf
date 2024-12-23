9ea512a8704bd2d26c013e8edf01ec28b9c5c507
Pavan Kalyan <kpavankalyan534@gmail.com>
2024-12-23 13:15:10 +0530

    fix

--- s3.tf
+++ s3.tf
@@ -1,23 +1,22 @@
-# 1. Create the S3 Bucket for Terraform state storage
+# Define S3 Bucket
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "pavan-terraform-state"  # Change this to a unique name
+  bucket = "pavan-terraform-state"  # Change to a unique name
 
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 }
 
-# 2. Enable Versioning on the S3 Bucket
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+# Enable Versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
-
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-# 3. Enable Encryption on the S3 Bucket
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+# Enable Encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
 
   rule {
@@ -27,17 +26,16 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
   }
 }
 
-# 4. Block Public Access on the S3 Bucket
-resource "aws_s3_bucket_public_access_block" "terraform_state_public_access_block" {
-  bucket = aws_s3_bucket.terraform_state.id
-
+# Block Public Access for the S3 bucket
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket                  = aws_s3_bucket.terraform_state.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-# 5. Create the DynamoDB Table for State Locking
+# Create DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_state" {
   name         = "terraform-state"
   billing_mode = "PAY_PER_REQUEST"
@@ -46,17 +44,5 @@ resource "aws_dynamodb_table" "terraform_state" {
   attribute {
     name = "LockID"
     type = "S"
-  }
-}
-
-# 6. Use local-exec provisioner to reinitialize Terraform backend after provisioning
-resource "null_resource" "backend_init" {
-  depends_on = [
-    aws_s3_bucket.terraform_state,
-    aws_dynamodb_table.terraform_state
-  ]
-
-  provisioner "local-exec" {
-    command = "terraform init -backend-config=\"bucket=${aws_s3_bucket.terraform_state.bucket}\" -backend-config=\"key=global/s3/terraform.tfstate\" -backend-config=\"region=ap-south-1\" -backend-config=\"encrypt=true\" -backend-config=\"dynamodb_table=terraform-state\""
   }
 }
